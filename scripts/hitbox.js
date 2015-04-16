ab08ae945171a53192cbb562d608286173a07893
Sebastien Dancer-Michel <s.dancermichel@gmail.com>
2015-04-16 15:43:41 +0200

    Thursday afternoon update
    Death detector functionning, still work to do on stopping the game.
    
    Also, ùdkvnzodvbaàoebvapeivbezdfpvizbevpzebaibdevapeisvbzdûvbf^puvaberpv

--- scripts/hitbox.js
+++ scripts/hitbox.js
@@ -1,4 +1,4 @@
-
+var etatPerso = true;
 function hitboxCheck() {
 	var perso = $('#perso');
 	var persoHitbox = {
@@ -23,9 +23,10 @@ function hitboxCheck() {
 	if(obsHitbox.posleft <= persoHitbox.posright){
 		if(persoHitbox.posbottom <= obsHitbox.postop){
 			console.log('DEAD MODAFUCKA');
-		} else {
-			obs.removeAttr('id');
 			// Rajouter code de fin de jeu ici !
+			var etatPerso = false;
+			console.log(etatPerso);
+			obs.removeAttr('id');
 		}
 	}
 	window.setTimeout(hitboxCheck, 500);
@@ -33,44 +34,6 @@ function hitboxCheck() {
 hitboxCheck();
 
 
-
-
-
-// var overlap = (function () {
-//     function getPositions( elem ) {
-//         var postop, postleft, width, height;
-//         postop = parseInt($( elem ).css('top'));
-//         posleft = parseInt($( elem ).css('left'));
-//         width = $( elem ).width();
-//         height = $( elem ).height();
-//         return [ [ posleft, posleft + width ], [ postop, postop + height ] ];
-//     }
-
-//     function comparePositions( p1, p2 ) {
-//         var r1, r2;
-//         r1 = p1[0] < p2[0] ? p1 : p2;
-//         r2 = p1[0] < p2[0] ? p2 : p1;
-//         return r1[1] > r2[0] || r1[0] === r2[0];
-//     }
-
-//     return function ( a, b ) {
-//         var pos1 = getPositions( a ),
-//             pos2 = getPositions( b );
-//         return comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] );
-//     };
-// })();
-
-
-
-// function overlapCheck() {
-// 	$('body').each(function(){
-// 		var perso = $('#perso')[0];
-// 		var obstacle = $('.obstacle')[0];    
-// 		if (overlap(obstacle, perso)){
-// 		    console.log('overlapping !');
-// 		};
-// 		// console.log('overlapping !'); // le timer marche bien !
-// 	window.setTimeout(overlapCheck, 100);
-//     })
-// };
-// overlapCheck();
+if(etatPerso = false){
+	window.alert('DEAD');
+}
